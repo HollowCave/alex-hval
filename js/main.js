cded5e98485dc5f88b0e3a13e2ad404a4c53122e
Alex Hval <alexhval@juno.com>
2019-09-08 14:39:18 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ const button = document.querySelector('.button'),
       backButton = document.querySelector('.back-btn'),
       about = document.querySelector('.about'),
       experience = document.querySelector('.experience'),
-      contact = document.querySelector('.contact')
+      contact = document.querySelector('.contact');
+
+about.style.display = 'none';
+experience.style.display = 'none';
+contact.style.display = 'none';
+
+document.addEventListener('load', () => {
+  about.style.display = 'block';
+  experience.style.display = 'block';
+  contact.style.display = 'block';
+});
 
 
 document.addEventListener('click', (event) => {
